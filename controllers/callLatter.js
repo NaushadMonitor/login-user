c6f41fcee861a71732e7349269bbc752755efd3a
Naushad Alam <code-react>
2023-11-03 23:39:35 +0530

    first commit

--- controllers/callLatter.js
+++ controllers/callLatter.js
@@ -5,20 +5,17 @@ const ErrorHandler = require("../utils/errorhander");
 const { pagination } = require("../utils/apifeatures");
 
 exports.addLatter = catchAsyncErrors(async (req, res, next) => {
-  
   if (!req.files || !req.files.latter) {
-    return next(
-      new ErrorHandler(`please select a file`, 400)
-    );
+    return next(new ErrorHandler(`please select a file`, 400));
   }
   const latter = req.files.latter;
   const myCloud = await cloudinary.v2.uploader.upload(latter.tempFilePath, {
     folder: "latter",
-    resource_type: 'auto'
+    resource_type: "auto",
   });
 
   await Latter.create({
-    reference_no:req.body.reference_no,
+    reference_no: req.body.reference_no,
     latter: {
       public_id: myCloud.public_id,
       url: myCloud.secure_url,
@@ -31,16 +28,16 @@ exports.addLatter = catchAsyncErrors(async (req, res, next) => {
   });
 });
 
-
-
 exports.getLatter = catchAsyncErrors(async (req, res, next) => {
-  const page = parseInt(req.query.page) || 1; 
-  const resultPerPage = parseInt(req.query.perPage) || 10; 
+  const page = parseInt(req.query.page) || 1;
+  const resultPerPage = parseInt(req.query.perPage) || 10;
 
   const latter = await pagination(Latter, page, resultPerPage);
 
   if (latter.results.length === 0) {
-    return next(new ErrorHandler(`No applications found for page ${page}`, 404));
+    return next(
+      new ErrorHandler(`No applications found for page ${page}`, 404)
+    );
   }
 
   res.status(200).json({
@@ -49,3 +46,22 @@ exports.getLatter = catchAsyncErrors(async (req, res, next) => {
   });
 });
 
+exports.deleteLatter = catchAsyncErrors(async (req, res, next) => {
+  const latter = await Latter.findById(req.params.id);
+
+  if (!latter) {
+    return next(
+      new ErrorHandler(
+        `Call Latter does not exist with Id: ${req.params.id}`,
+        400
+      )
+    );
+  }
+
+  await latter.remove();
+
+  res.status(200).json({
+    success: true,
+    message: "Call Latter Deleted Successfully",
+  });
+});
--- routes/callLatter.js
+++ routes/callLatter.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { addLatter, getLatter } = require("../controllers/callLatter");
+const { addLatter, getLatter, deleteLatter } = require("../controllers/callLatter");
 
 const router = express.Router();
 
 router.route("/add-latter").post(addLatter);
 router.route("/all-latter").get(getLatter);
+router.route("/delete/:id").delete(deleteLatter);
 
 module.exports = router;
